b6bdc9584dd71e7ff1f802764219c0279c4dd430
Haozhe Xu <jamesxuhaozhe@gmail.com>
2018-12-10 18:01:15 +0800

    Learned variable definition

---
+++ src/main/scala/com/haozhexu/scala/Variables.scala
@@ -0,0 +1,17 @@
+package com.haozhexu.scala
+
+object Variables {
+
+  var variable1: Int = 2
+
+  var variable2 = 3
+
+  var variable3: String = "Hello World"
+
+  val variable4 = 1
+
+  val variable5: Int = 2
+
+  var variable6: Double = 4
+
+}
