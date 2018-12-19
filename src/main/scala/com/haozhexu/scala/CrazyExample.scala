e901caf5cea2da0376bc6b6775cb21e3810ddce0
Haozhe Xu <jamesxuhaozhe@gmail.com>
2018-12-19 23:49:58 +0800

    Add example for call by name and call by value, by name parameter, lazy eval

---
+++ src/main/scala/com/haozhexu/scala/CrazyExample.scala
@@ -0,0 +1,43 @@
+package com.haozhexu.scala
+
+import scala.io.Source
+import scala.util.control.NonFatal
+
+object CrazyExample {
+  def main(args: Array[String]): Unit = {
+    args foreach(arg => countLines(arg))
+  }
+
+  object manage {
+    def apply[R <: { def close(): Unit}, T](resource: => R)(f: R => T) = {
+      var res: Option[R] = None
+      try {
+        res = Some(resource)
+        f(res.get)
+      } catch {
+        case NonFatal(ex) => println(s"Non Fatal exception! &ex")
+      } finally {
+        if (res != None) {
+          println(s"Closing resource...")
+          res.get.close
+        }
+      }
+    }
+  }
+
+  def countLines(fileName: String) = {
+    println()
+    manage(Source.fromFile(fileName)) { source =>
+      val size = source.getLines.size
+      println(s"file $fileName has $size lines")
+      if (size > 20) throw new RuntimeException("Big file!")
+    }
+  }
+
+  lazy val resource: Int = init()
+
+  def init(): Int = {
+    0
+  }
+
+}
